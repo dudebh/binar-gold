2d5992d29277de564c837316cabb447ae054fd7c
Dude Bahrul Hayat <79790420+dudebh@users.noreply.github.com>
2022-10-24 11:07:00 +0700

    fix chart

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import re
-
-from flask import Flask, jsonify, make_response, send_file
+from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
@@ -12,8 +11,8 @@ import pandas as pd
 import chardet
 import re
 import matplotlib.pyplot as plt
-import numpy as np
 import base64
+import sqlite3
 
 fileKamus = 'resource/new_kamusalay.csv'
 app.json_encoder = LazyJSONEncoder
@@ -39,6 +38,7 @@ swagger_config = {
     "enableCORS": False,
 }
 swagger = Swagger(app, template=swagger_template,config=swagger_config)
+
 @cross_origin()
 
 @swag_from("docs/dataclean.yml", methods=['POST'])
@@ -54,9 +54,6 @@ def dataclean():
             cleanedRowText = cleanSlang(row['Tweet'])
             cleanedData.append(cleanedRowText)
         chartImage = createChart(dfFromFile)
-        # response = make_response(send_file(chartImage,mimetype='image/png'))
-        # response.headers['Content-Transfer-Encoding']='base64'
-        # return response
         return jsonify({'text': '\n'.join(cleanedData), 'image': chartImage.decode('utf-8')})
     else:
         print(request.form)
@@ -75,13 +72,24 @@ def cleanText(text):
 
 def cleanSlang(text):
     text = cleanText(text)
-    dfSlang = pd.read_csv(fileKamus, encoding=checkDataType(fileKamus))
-    dicSlang = dict(zip(dfSlang.slang, dfSlang.formal))
+    #dfSlang = pd.read_csv(fileKamus, encoding=checkDataType(fileKamus))
+    dfSlang = pd.read_sql_query("SELECT Slang, Formal from SlangDictionary", sqlConn())
+    dicSlang = dict(zip(dfSlang.Slang, dfSlang.Formal))
     words = []
     for word in text.split(' '):
         if word in dicSlang.keys():
             word = dicSlang[word]
         words.append(word)
+    newWords = cleanStopWord(' '.join(words))
+    return newWords
+
+def cleanStopWord(text):
+    dfStopWord = pd.read_sql_query("SELECT Word from StopWords", sqlConn())
+    listStopWord = dfStopWord["Word"].tolist()
+    words = []
+    for word in text.split(' '):
+        if word not in listStopWord:
+            words.append(word)
     return ' '.join(words)
 
 def checkDataType(file):
@@ -95,8 +103,39 @@ def createChart(df):
 
     abusive = df["Abusive"].tolist()
     countAbusive = pd.DataFrame(abusive).sum()[0]
-    headers = ['hate_speech', 'abusive']
-    values = [countHateSpeech, countAbusive]
+
+    hsIndividual = df["HS_Individual"].tolist()
+    countHsIndividual = pd.DataFrame(hsIndividual).sum()[0]
+
+    hsGroup = df["HS_Group"].tolist()
+    countHsGroup = pd.DataFrame(hsGroup).sum()[0]
+
+    hsReligion = df["HS_Religion"].tolist()
+    countHsReligion = pd.DataFrame(hsReligion).sum()[0]
+
+    hsRace = df["HS_Race"].tolist()
+    countHsRace = pd.DataFrame(hsRace).sum()[0]
+
+    hsPhysical = df["HS_Physical"].tolist()
+    countHsPhysical = pd.DataFrame(hsPhysical).sum()[0]
+
+    hsGender = df["HS_Gender"].tolist()
+    countHsGender = pd.DataFrame(hsGender).sum()[0]
+
+    hsOther = df["HS_Other"].tolist()
+    countHsOther = pd.DataFrame(hsOther).sum()[0]
+
+    hsWeak = df["HS_Weak"].tolist()
+    countHsWeak = pd.DataFrame(hsWeak).sum()[0]
+
+    hsModerate = df["HS_Moderate"].tolist()
+    countHsModerate = pd.DataFrame(hsModerate).sum()[0]
+
+    hsStrong = df["HS_Strong"].tolist()
+    countHsStrong = pd.DataFrame(hsStrong).sum()[0]
+
+    headers = ['hate_speech', 'abusive', 'HS_Individual','HS_Group','HS_Religion','HS_Race','HS_Physical','HS_Gender','HS_Other','HS_Weak','HS_Moderate','HS_Strong']
+    values = [countHateSpeech, countAbusive, countHsIndividual, countHsGroup, countHsReligion, countHsRace, countHsPhysical, countHsGender, countHsOther, countHsWeak, countHsModerate, countHsStrong ]
     dfChart = pd.DataFrame({
         'Name': headers,
         'Point': values
@@ -109,5 +148,10 @@ def encodeFile(fileName):
     with open(fileName, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     return encoded_string
+
+def sqlConn():
+    con = sqlite3.connect("cleansing_db.db")
+    return con
+
 if __name__ == '__main__':
     app.run()
